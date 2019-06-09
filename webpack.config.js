bc84c217f7969081ffebc784eb35781135358fc3
MaruBlare <marinamaru@bk.ru>
2019-06-10 02:58:17 +0300

    Sfec-0. Подключила плагин для генерации иконок.

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 
 module.exports = {
   entry: {
@@ -31,7 +32,14 @@ module.exports = {
             'postcss-loader', 
             'sass-loader'
         ]
-      }
+      },
+      {
+        test: /\.(jpg|png|svg)$/,
+        loader: 'file-loader',
+        options: {
+            name: 'images/[name].[ext]'
+        },
+      },
     ]
   },
   plugins: [ 
@@ -46,6 +54,37 @@ module.exports = {
         filename: 'index.html'
     }),
     new WebpackMd5Hash(),
+    /*new FaviconsWebpackPlugin({
+      logo: './src/logo.svg',
+      title: 'Homepage',
+      icons: {
+        android: false,
+        appleIcon: false,
+        appleStartup: false,
+        coast: false,
+        favicons: true,
+        firefox: false,
+        opengraph: false,
+        twitter: false,
+        yandex: false,
+        windows: false
+      }
+    })*/
+    new FaviconsWebpackPlugin({
+      logo:'./src/logo.png',
+      icons: {
+        android: false,
+        appleIcon: false,
+        appleStartup: false,
+        coast: false,
+        favicons: true,
+        firefox: false,
+        opengraph: false,
+        twitter: false,
+        yandex: false,
+        windows: false
+      }
+    }),
 
   ],
   devServer: {
