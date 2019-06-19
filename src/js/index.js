71b48f7173589cac11c45245f631a17905bd9168
MaruBlare <marinamaru@bk.ru>
2019-06-19 23:05:41 +0300

    Sfec-1. WIP. Изменила подход к стилизации элементов на странице. Убрала из хедера блоки, которые должны быть в секции с контентом. Разбираюсь с выравниванием элементов хедера.
    Sfec-1. Добавила hover для ссылок (первоначальный вариант).
    
    Sfec-1. Сделала хедер с липким позиционированием, добавила изменение background-color при скролле.

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,15 @@
 import "../scss/style.scss";
-import Bootstrap from 'bootstrap/dist/css/bootstrap.css'; 
 import $ from 'jquery';
+
+$(document).ready(function () {
+  var headerHeight = 88;
+  var ultramatineBlue = "rgb(42, 75, 255, .1)"
+
+  $(window).on("scroll", function () {
+    if ($(window).scrollTop() > headerHeight) {
+      $('.header').css('background', ultramatineBlue);
+    } else {
+      $('.header').css('background', "none");
+    }
+  });
+});
