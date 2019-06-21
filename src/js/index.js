0dfe182875a06efd254508b41fb4d68f8254022f
MaruBlare <marinamaru@bk.ru>
2019-06-22 02:34:47 +0300

    Sfec-1. Убрала лишние классы container. Изменила вид хедера при скролле. Исправила медиазапросы для отзычивости шрифта и отступов между блоками.

--- src/js/index.js
+++ src/js/index.js
@@ -3,17 +3,13 @@ import $ from 'jquery';
 
 $(document).ready(function () {
   var headerHeight = 88;
-  var ultramatineBlue = "rgb(42, 75, 255, .4)";
-  var white = "rgb(255, 255, 255)";
-  var charcoalBlack = "rgb(14, 15, 33)";
-
   $(window).on("scroll", function () {
     if ($(window).scrollTop() > headerHeight) {
-      $('.header .container').css('background', ultramatineBlue);
-      $('.header-logo-text').css('color', white);
+      $('.header .container').addClass( "on-scroll-header" );
+      $('nav .nav-link').addClass( "on-scroll-link" );
     } else {
-      $('.header .container').css('background', "none");
-      $('.header-logo-text').css('color', charcoalBlack);
+      $('.header .container').removeClass( "on-scroll-header" );
+      $('nav .nav-link').removeClass( "on-scroll-link" );
     }
   });
 });
