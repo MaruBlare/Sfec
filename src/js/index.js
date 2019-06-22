bbf940a5dac708e71e69f7cf8c84da34433f6910
MaruBlare <marinamaru@bk.ru>
2019-06-22 21:01:10 +0300

    Sfec-1. Поправила размеры контейнера, изменила подход к инвертированию цвета ссылок навигации (теперь цвет меняет и иконка корзины).

--- src/js/index.js
+++ src/js/index.js
@@ -5,11 +5,11 @@ $(document).ready(function () {
   var headerHeight = 88;
   $(window).on("scroll", function () {
     if ($(window).scrollTop() > headerHeight) {
-      $('.header .container').addClass( "on-scroll-header" );
-      $('nav .nav-link').addClass( "on-scroll-link" );
+      $('.header-container').addClass( "on-scroll-header" );
+      $('nav .nav-link').addClass( "invert" );
     } else {
-      $('.header .container').removeClass( "on-scroll-header" );
-      $('nav .nav-link').removeClass( "on-scroll-link" );
+      $('.header-container').removeClass( "on-scroll-header" );
+      $('nav .nav-link').removeClass( "invert" );
     }
   });
 });
