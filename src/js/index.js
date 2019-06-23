b451fa76637df0e049152c0a3c8869582a95bd59
MaruBlare <marinamaru@bk.ru>
2019-06-23 23:19:44 +0300

    Sfec-1. Убрала лишние контейнеры у хедера и футера. Изменила реализацию лого.

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,15 @@
 import "../scss/style.scss";
 import $ from 'jquery';
+import './blog';
 
 $(document).ready(function () {
   var headerHeight = 88;
   $(window).on("scroll", function () {
     if ($(window).scrollTop() > headerHeight) {
-      $('.header-container').addClass( "on-scroll-header" );
+      $('.header-wrapper').addClass( "on-scroll-header" );
       $('nav .nav-link').addClass( "invert" );
     } else {
-      $('.header-container').removeClass( "on-scroll-header" );
+      $('.header-wrapper').removeClass( "on-scroll-header" );
       $('nav .nav-link').removeClass( "invert" );
     }
   });
