c7aa02b07611b63d9d41cf7d4435572bd10e448a
MaruBlare <marinamaru@bk.ru>
2019-06-27 23:47:53 +0300

    Базовые стили для элементов, а также для хереда/футера.

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,16 @@
 import "../scss/style.scss";
-import Bootstrap from 'bootstrap/dist/css/bootstrap.css'; 
 import $ from 'jquery';
+import './index';
+
+$(document).ready(function () {
+  var headerHeight = 88;
+  $(window).on("scroll", function () {
+    if ($(window).scrollTop() > headerHeight) {
+      $('.header').addClass( "on-scroll-header" );
+      $('nav .nav-link').addClass( "invert" );
+    } else {
+      $('.header').removeClass( "on-scroll-header" );
+      $('nav .nav-link').removeClass( "invert" );
+    }
+  });
+});
