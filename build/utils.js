e67361380617b44fc847e46489f7264a0a0e9f99
MaruBlare <marinamaru@bk.ru>
2019-06-30 23:38:44 +0300

    Изменила index.html: добавила кнопки для перехода между теми страницами, которые будут реализованы в рамках курса.

--- build/utils.js
+++ build/utils.js
@@ -12,7 +12,7 @@ exports.pages = function (env, folder = '') {
       return false;
 
     const viewName = view.split('.')[0];
-    const fileName = folder === '' ? `assets/views/${viewName}/index.html` : `assets/views/${folder}/${viewName}/index.html`;
+    const fileName = folder === '' ? `${viewName}/index.html` : `${folder}/${viewName}/index.html`;
     const options = {
       filename: fileName,
       template: `${rootPagesFolderName}/${folder}/${view}`,
