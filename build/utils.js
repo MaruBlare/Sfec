0ef7c6e8a4432956024baea9465f1bc2ee362f7a
MaruBlare <marinamaru@bk.ru>
2019-07-02 01:30:50 +0300

    Вернулась к логике, что главной страницей является homepage. Сделала хедер для планшетов и мобильных (меню-гамбургер).

--- build/utils.js
+++ build/utils.js
@@ -12,6 +12,7 @@ exports.pages = function (env, folder = '') {
       return false;
 
     const viewName = view.split('.')[0];
+    if (viewName[0] + viewName[1] == 'el') return;
     const fileName = folder === '' ? `${viewName}/index.html` : `${folder}/${viewName}/index.html`;
     const options = {
       filename: fileName,
